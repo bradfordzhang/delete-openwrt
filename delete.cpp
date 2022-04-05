b979685d3fd05cf49a9911cdcf314f1393130e20
SkyTeamWater <zyc@zyc.name>
2022-04-05 18:21:15 +0800

    add delete.cpp file

---
+++ delete.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    freopen("/var/www/openwrt/self/index.text", "r", stdin);
+    char date[10], file[50], week;
+    while (scanf("%s %c %s", date, &week, file) != EOF) {
+        if ((!((date[0] == '0' && date[1] == '1') || week == '1'))){
+            string tmp=file;
+            cout<<"sudo rm -rf /var/www/openwrt/self/"+tmp<<endl;
+        }
+    }
+    return 0;
+}
