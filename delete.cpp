b8d31f6fbb881bb5be08e4dbe71abacefa2f2b31
Bradford Zhang <zyc@zyc.name>
2022-05-22 18:37:07 +0800

    delete weekly version

--- delete.cpp
+++ delete.cpp
@@ -6,7 +6,7 @@ int main() {
     freopen("/var/www/openwrt/self/index.text", "r", stdin);
     char date[10], file[50], week;
     while (scanf("%s %c %s", date, &week, file) != EOF) {
-        if ((!((date[0] == '0' && date[1] == '1') || week == '1'))) {
+        if (!(date[0] == '0' && date[1] == '1')) {
             string tmp = file;
             cout << "sudo rm -rf /var/www/openwrt/self/" + tmp << endl;
         }
