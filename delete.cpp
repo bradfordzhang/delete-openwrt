8caa4edd4b63d2dd727a78431bedbe20f636e5aa
Bradford Zhang <zyc@zyc.name>
2022-12-08 17:03:35 +0800

    add array size
    Signed-off-by: Bradford Zhang <zyc@zyc.name>

--- delete.cpp
+++ delete.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main() {
     freopen("/var/www/openwrt/self/index.text", "r", stdin);
-    char date[10], file[50], week;
+    char date[50], file[150], week;
     while (scanf("%s %c %s", date, &week, file) != EOF) {
         if ( !(date[0] == '0' && date[1] == '1') && (strstr(file, "-v")==NULL ) ) {
             string tmp = file;
