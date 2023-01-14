91b9510341ba9ee6acde5928e0f43f111a1049cd
Bradford Zhang <zyc@zyc.name>
2023-01-14 18:18:06 +0800

    add delete release version
    Signed-off-by: Bradford Zhang <zyc@zyc.name>

--- delete.cpp
+++ delete.cpp
@@ -7,7 +7,7 @@ int main() {
     freopen("/var/www/openwrt/self/index.text", "r", stdin);
     char date[50], file[150], week;
     while (scanf("%s %c %s", date, &week, file) != EOF) {
-        if ( !(date[0] == '0' && date[1] == '1') && (strstr(file, "-v")==NULL ) ) {
+        if ( !(date[0] == '0' && date[1] == '1') /*&& (strstr(file, "-v")==NULL )*/ ) {
             string tmp = file;
             cout << "sudo rm -rf /var/www/openwrt/self/" + tmp << endl;
         }
