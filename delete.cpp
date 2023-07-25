2ca8366842f9fac20f3f34584c0d3cef32885156
Bradford Zhang <zyc@zyc.name>
2023-07-25 19:01:12 +0800

    change delete mode
    Signed-off-by: Bradford Zhang <zyc@zyc.name>

--- delete.cpp
+++ delete.cpp
@@ -1,16 +1,39 @@
 #include <cstdio>
-#include <iostream>
+#include <cstdlib>
 #include <cstring>
-#include <string>
-using namespace std;
+#include <ctime>
+
+void checkIfThursday() {
+    time_t timer;
+    struct tm *Now;
+    time(&timer);
+    Now = localtime(&timer);
+    if ((*Now).tm_wday + 1 != 4) {
+        printf("Not Thursday");
+        exit(0);
+    }
+}
+
+void delete_dir(const char *dir_path) {
+    char cmd[512];
+    sprintf(cmd, "sudo rm -rf %s", dir_path);
+    system(cmd);
+}
+
 int main() {
-    freopen("/var/www/openwrt/self/index.text", "r", stdin);
+    checkIfThursday();
+    FILE *index = NULL;
+    index = fopen("/var/www/openwrt/self/index.text", "r+");
     char date[50], file[150], week;
-    while (scanf("%s %c %s", date, &week, file) != EOF) {
-        if ( !(date[0] == '0' && date[1] == '1') /*&& (strstr(file, "-v")==NULL )*/ ) {
-            string tmp = file;
-            cout << "sudo rm -rf /var/www/openwrt/self/" + tmp << endl;
+    while (fscanf(index, "%s %c %s", date, &week, file) != EOF) {
+        if (!(date[0] == '0' && date[1] == '1') || (strstr(file, "snapshot") != NULL)) {
+            char filepath[512];
+            sprintf(filepath, "/var/www/openwrt/self/%s\n", file);
+            delete_dir(filepath);
         }
     }
+    fclose(index);
+    index = fopen("/var/www/openwrt/self/index.text", "w");
+    fclose(index);
     return 0;
 }
